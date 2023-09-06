37ee65cf8534fe276fbe4cd630b103d0d375f88e
MohammedMaher177 <anahammo777@gmail.com>
2023-09-06 16:19:20 +0300

    firestcommi

---
+++ src/Components/layout/Layout.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import NavBar from '../Navbar/NavBar.jsx'
+
+export default function Layout() {
+    return (
+        <>
+            <NavBar />
+            <div className="container py-5">
+                <Outlet></Outlet>
+            </div>
+
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './Components/layout/Layout.jsx';
+import Home from './Components/Home/Home.jsx';
+import { Provider } from 'react-redux';
+import store from './Redux/Store.js';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> }
+    ]
+  }
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
