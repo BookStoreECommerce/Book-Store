3a0238ddeb0f7fe39527371508d66af578cf6c43
Aya-Adel-Mohamed <aya90318@gmail.com>
2023-09-11 15:49:33 -0700

    reusable component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './Components/layout/Layout.jsx';
 import Home from './Components/Home/Home.jsx';
