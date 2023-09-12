bc8c414db735f48b6c9d848235b8d179938b5941
marwa3ttia <marwaattia871@gmail.com>
2023-09-12 10:38:19 +0200

    shared css for button and input

--- src/Components/layout/Layout.jsx
+++ src/Components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import NavBar from '../Navbar/NavBar.jsx'
+import NavBar from '../Navbar/NavBar.jsx';
 
 export default function Layout() {
     return (
