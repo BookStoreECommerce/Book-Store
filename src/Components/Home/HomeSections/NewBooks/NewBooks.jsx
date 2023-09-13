45fbcb2e41fee49cb35f37074613aa8e7e214540
WalaaRahman <w.gohary46@gmail.com>
2023-09-13 16:20:54 +0200

    newBooks

--- src/Components/Home/HomeSections/NewBooks/NewBooks.jsx
+++ src/Components/Home/HomeSections/NewBooks/NewBooks.jsx
@@ -1,134 +1,162 @@
-import React,{useEffect, useState} from "react";
+import React, { useState } from "react";
 import BookList from "../../../ReusableComponents/BookList/BookList";
 import styles from '../NewBooks/NewBooks.module.css';
-import book from '../../../../assets/10.jpg'
-import book2 from '../../../../assets/3.jpg'
-import book3 from '../../../../assets/6.jpg'
-import book4 from '../../../../assets/11.jpg'
+import book from '../../../../assets/1.jpg'
+import book2 from '../../../../assets/2.jpg'
+import book3 from '../../../../assets/3.jpg'
+import book4 from '../../../../assets/4.jpg'
+import book5 from '../../../../assets/5.jpg'
+import book6 from '../../../../assets/6.jpg'
+import book7 from '../../../../assets/7.jpg'
+import book8 from '../../../../assets/8.jpg'
+import book9 from '../../../../assets/9.jpg'
+import book10 from '../../../../assets/10.jpg'
+import book11 from '../../../../assets/11.jpg'
+import book12 from '../../../../assets/12.jpg'
+
 
 const NewBooks = () => {
     const [NewBooks, setNewBooks] = useState([
         {
-            id:0,
+            id: 0,
             image: book,
             name: "War of Dragon",
             price: 350
         },
         {
-            id:1,
-            image:book4,
+            id: 1,
+            image: book2,
             name: "Alone Walker",
             price: 105
         },
         {
-            id:2,
-            image:book3,
+            id: 2,
+            image: book3,
             name: "Moon Light Srdow",
             price: 200
         }
         ,
         {
-            id:3,
-            image:book2,
+            id: 3,
+            image: book4,
             name: "Animals Life",
             price: 400
         }
         ,
         {
-            id:4,
-            image:book2,
+            id: 4,
+            image: book5,
             name: "Animals Life",
             price: 400
         }
         ,
         {
-            id:5,
-            image:book2,
+            id: 5,
+            image: book6,
             name: "Animals Life",
             price: 400
         }
         ,
         {
-            id:6,
-            image:book2,
+            id: 6,
+            image: book7,
             name: "Animals Life",
             price: 400
         }
         ,
         {
-            id:7,
-            image:book2,
+            id: 7,
+            image: book8,
             name: "Animals Life",
             price: 400
         }
         ,
         {
-            id:8,
-            image:book2,
+            id: 8,
+            image: book9,
             name: "Animals Life",
             price: 400
         }
         ,
         {
-            id:9,
-            image:book2,
+            id: 9,
+            image: book10,
             name: "Animals Life",
             price: 400
         }
         ,
         {
-            id:10,
-            image:book2,
+            id: 10,
+            image: book11,
             name: "Animals Life",
             price: 400
         }
         ,
         {
-            id:11,
-            image:book2,
+            id: 11,
+            image: book12,
             name: "Animals Life",
             price: 400
         }
     ])
     const [startIndex, setStart] = useState(0);
     const [lastIndex, setLast] = useState(4);
-    // let lastIndex=4;
-    let cutNewBooks = NewBooks.slice(startIndex,lastIndex);
-    
-    // const [newArray , setNewArray] = useState(cutNewBooks);
-    console.log("start: ",startIndex);
-    console.log("end: ",lastIndex);
-    const shiftRight = ()=>{
-        setStart((prev) => prev + 1 )
-        setLast((prev) => prev + 1 )
-        console.log("Right");
-        // setNewArray(NewBooks.slice(startIndex,lastIndex))
-    }
-    const shiftLeft = ()=>{
-        setStart((prev) => prev - 1 )
-        setLast((prev) => prev - 1 )
-        console.log("Left");
-        // setNewArray(NewBooks.slice(startIndex,lastIndex))
+    const [leftBtn, setLeftBtn] = useState(true);
+    const [rightBtn, setRightBtn] = useState(false);
+    let cutNewBooks = NewBooks.slice(startIndex, lastIndex);
+    // console.log("start: ",startIndex);
+    // console.log("end: ",lastIndex);
+
+    const shiftRight = () => {
+        setStart((prev) => prev + 1)
+        setLast((prev) => prev + 1)
+        setLeftBtn(false)
+
+
+        if (lastIndex === NewBooks.length - 1) {
+            setRightBtn(true)
+
+        }
+      
+
     }
 
-//     useEffect(()=>{
-// console.log(newArray);
-//     },[newArray])
+    const shiftLeft = () => {
+        setStart((prev) => prev - 1)
+        setLast((prev) => prev -= 1)
+        console.log(startIndex);
+        if (startIndex - 1 <= 0) {
+            setLeftBtn(true)
 
-    return ( 
-        <>
-        <div className="container">
-            <div className={`row justify-content-center align-items-center text-center ${styles.gap}`}>
-            <h2 className="blueHeader">New Arrivals</h2>
-            <div className="d-flex justify-content-end">
-                <button className={`${styles.btn} ${styles.leftBtn}`} onClick={shiftLeft}><i class="fa-solid fa-arrow-left"></i></button>
-                <button className={`${styles.btn} ${styles.rightBtn}`} onClick={shiftRight}><i class="fa-solid fa-arrow-right"></i></button>
-            </div>
-            <BookList NewBooks = {cutNewBooks} sectionName={"newBooks"}/>
-            </div>
-        </div>
-        </>
-     );
-}
+        }
+        if(lastIndex -1 <= NewBooks.length - 1){
+            setRightBtn(false)
+
+        }
+
+    }
  
+    return (
+        <>
+
+            <div className="container">
+                <div className={`row justify-content-center align-items-center text-center ${styles.gap}`}>
+                    <h2 className="blueHeader">New Arrivals</h2>
+                    <div className="d-flex justify-content-end">
+                        <button className={`${leftBtn ? styles.disabled : styles.enabled} ${styles.btn} ${styles.leftBtn} `} id="leftBtn" onClick={shiftLeft} ><i class="fa-solid fa-arrow-left"></i></button>
+                        <button className={`${rightBtn ? styles.disabled : styles.enabled} ${styles.btn} `} id="righttBtn" onClick={shiftRight}><i class="fa-solid fa-arrow-right"></i></button>
+                    </div>
+                    <BookList NewBooks={cutNewBooks} sectionName={"newBooks"} />
+                </div>
+            </div>
+
+
+
+
+
+
+        </>
+    );
+}
+
 export default NewBooks;
--- src/Components/ReusableComponents/BookCard/BookCard.jsx
+++ src/Components/ReusableComponents/BookCard/BookCard.jsx
@@ -6,8 +6,8 @@ import styles from './BookCard.module.css'
 const BookCard = ({image, name , price , section}) => {
     return ( 
 <>
-    <div className="col-lg-2 col-md-4 col-xs-1 text-center">
-        <div className={`mb-2 position-relative`}> 
+    <div className="col-lg-3 col-md-4 col-xs-1 text-center">
+        <div className={`mb-2 position-relative ${styles.imgContainer}`}> 
             <img src= {image} className={`w-100 ${styles.cardImg}`}   alt="" />
         </div>
         <div className="d-flex flex-column">
