6348ff34c06ef42a25f8228cc0176a8948bd36ac
MohamedElsayed2202 <mohamed.elsayed.1.3.1999@gmail.com>
2023-09-17 01:48:30 +0300

    adding messages to if con in interceptor

--- src/axios/axios-instance.jsx
+++ src/axios/axios-instance.jsx
@@ -1,5 +1,6 @@
 import { default as axios } from "axios";
 import { baseUrl } from "../util/util";
+import { redirect } from "react-router-dom";
 
 const axiosInstance = axios.create({
     baseURL: baseUrl
@@ -7,11 +8,14 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.response.use(res => res, async(error) => {
     // if(error.response.status === "403" && error.response.data.message)
-    if(error.response.status === "403"){
+    if(error.response.status === "403" && error.response.data.message === "jwt expired" || error.response.data.message === "access denied"){
             const response = await axiosInstance.post('auth/refresh', {}, { withCredentials: true });
             if(response.status === "201"){
                 axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${response.data.token}`;
                 return axiosInstance(error.config)
+            }
+            if(response.status === "401" || response.status === "403"){
+                //=> show a tost with this message "login timed out, please login again."
             }
     }
 
