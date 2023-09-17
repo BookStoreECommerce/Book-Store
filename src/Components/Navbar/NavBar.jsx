9bc7b55ee5e889c23406f684c6fd2f335348afb2
MohamedElsayed2202 <mohamed.elsayed.1.3.1999@gmail.com>
2023-09-17 13:02:37 +0300

    adding error element

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -4,14 +4,15 @@ import logo from "../../assets/logo.png";
 import styles from "./NavBar.module.css";
 import { useDispatch } from "react-redux";
 import { registerModal, loginModal } from "../../Redux/Slicies/dialogSlice";
+import { ref } from "yup";
 
-export default function NavBar() {
+export default function NavBar({innerRef}) {
   const dispatch = useDispatch();
 
   return (
     <>
       <nav
-        className={`navbar fixed-top navbar-expand-lg py-3 ${styles.transparent}`}
+        className={`navbar fixed-top navbar-expand-lg py-3 ${styles.transparent}`} ref={innerRef}
       >
         <div className="container-fluid">
           <Link to="/" className={styles.logo}>
@@ -52,7 +53,7 @@ export default function NavBar() {
           >
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item me-2">
-                <Link className={`nav-link ${styles.navLink}`} to="home">
+                <Link className={`nav-link ${styles.navLink}`} to="/">
                   Home
                 </Link>
               </li>
