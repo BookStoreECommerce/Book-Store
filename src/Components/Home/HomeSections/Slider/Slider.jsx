8c551d4b4661dabc193a063531a2365ea240d46c
Aya-Adel-Mohamed <aya90318@gmail.com>
2023-09-19 14:02:21 -0700

    Aos

--- src/Components/Home/HomeSections/Slider/Slider.jsx
+++ src/Components/Home/HomeSections/Slider/Slider.jsx
@@ -20,12 +20,9 @@ const Slider = () => {
                         <div className={styles.carusouelContent}>
                             <h1 className={styles.headerContent}>Buy <span className={styles.colorHeader}>your</span><br /> favourite <span className={styles.colorHeader}>Book</span> <br />from <span className={styles.colorHeader}>Here</span></h1>
                             <button className={`btn px-lg-4 py-lg-2 ${styles.shopBtn}`}>Shop Now <i className="fa-solid fa-arrow-right ms-2 fw-bold"></i></button>
-
                         </div>
-                        <img src={slide1} className={`d-block w-100 ${styles.landscapeBg}`} alt="..." />
+                        <img src={slide1} className={`d-block w-100 ${styles.landscapeBg} ${styles.imgFlip}`} alt="..." />
                         <img src={bg1} className={`d-none w-100 ${styles.portritBg}`} alt="..." />
-
-
                     </div>
                     <div className={`carousel-item ${styles.sliderImgTwo}`}>
                         <div className={styles.carusouelContent}>
@@ -34,34 +31,29 @@ const Slider = () => {
                                 <br />
                                 in your <span className={styles.colorHeader}> Hands</span>
                             </h1>
-                    <button className={`btn px-lg-4 py-lg-2 ${styles.shopBtn}`}>Shop Now <i className="fa-solid fa-arrow-right ms-2 fw-bold"></i></button>
-                        
+                            <button className={`btn px-lg-4 py-lg-2 ${styles.shopBtn}`}>Shop Now <i className="fa-solid fa-arrow-right ms-2 fw-bold"></i></button>
                         </div>
-                        <img src={slide2} className={`d-block w-100 ${styles.landscapeBg}`} alt="..." />
+                        <img src={slide2} className={`d-block w-100 ${styles.landscapeBg} ${styles.imgFlip}`} alt="..." />
                         <img src={bg2} className={`d-none w-100 ${styles.portritBg}`} alt="..." />
-
-
                     </div>
                     <div className={`carousel-item  ${styles.sliderImgThree}`}>
                         <div className={styles.carusouelContent}>
                             <h1 className={styles.headerContent}>There is <span className={styles.colorHeader}>no</span><br /> friend as <span className={styles.colorHeader}>Loyal</span> <br />as a <span className={styles.colorHeader}>Book</span></h1>
-                    <button className={`btn px-lg-4 py-lg-2 ${styles.shopBtn}`}>Shop Now <i className="fa-solid fa-arrow-right ms-2 fw-bold"></i></button>
-                      
+                            <button className={`btn px-lg-4 py-lg-2 ${styles.shopBtn}`}>Shop Now <i className="fa-solid fa-arrow-right ms-2 fw-bold"></i></button>
                         </div>
-                        <img src={slide3} className={`d-block w-100 ${styles.landscapeBg}`} alt="..." />
+                        <img src={slide3} className={`d-block w-100 ${styles.landscapeBg} ${styles.imgFlip}`} alt="..." />
                         <img src={bg3} className={`d-none w-100 ${styles.portritBg}`} alt="..." />
                     </div>
                     <div className={`carousel-item ${styles.sliderImgFour}`}>
                         <div className={styles.carusouelContent}>
                             <h1 className={styles.headerContent}>Today a <span className={styles.colorHeader}>reader</span><br /> tomorrow a <span className={styles.colorHeader}>Leader</span> </h1>
-                    <button className={`btn px-lg-4 py-lg-2 ${styles.shopBtn}`}>Shop Now <i className="fa-solid fa-arrow-right ms-2 fw-bold"></i></button>
-                       
+                            <button className={`btn px-lg-4 py-lg-2 ${styles.shopBtn}`}>Shop Now <i className="fa-solid fa-arrow-right ms-2 fw-bold"></i></button>
                         </div>
-                        <img src={slide4} className={`d-block w-100 ${styles.landscapeBg}`} alt="..." />
+                        <img src={slide4} className={`d-block w-100 ${styles.landscapeBg} `} alt="..." />
                         <img src={bg4} className={`d-none w-100 ${styles.portritBg}`} alt="..." />
                     </div>
                 </div>
-                <SliderScrollBtn/>
+                <SliderScrollBtn />
             </div>
         </>
     );
