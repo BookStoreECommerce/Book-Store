4ff6e04e920cc3cc2758334f90cce3981af8be1d
Aya-Adel-Mohamed <aya90318@gmail.com>
2023-09-19 13:40:10 -0700

    Aos

--- src/Components/Home/HomeSections/AboutUs/AboutUs.jsx
+++ src/Components/Home/HomeSections/AboutUs/AboutUs.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styles from './AboutUs.module.css'
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 const AboutUs = () => {
-
+    useEffect(() => {
+        AOS.init();
+        window.addEventListener('load', AOS.refresh);
+    }, []);
     return (
         <>
             <section className={` ${styles.bg} darkBg `}>
@@ -12,7 +16,7 @@ const AboutUs = () => {
                             <div className="header">
                                 <h2 className={`${styles.AboutHeader} mt-5 text-center text-white`}>About Us</h2>
                             </div>
-                            <div className="col-12">
+                            <div className="col-12" data-aos="zoom-in">
                                 <p className={`${styles.parg}`}>
                                     Since its inception, SAYEGH is committed to the advancement of culture and education,
                                     through the provision of quality educational content,
--- src/Components/Home/HomeSections/CategoryCard/CategoryCard.jsx
+++ src/Components/Home/HomeSections/CategoryCard/CategoryCard.jsx
@@ -1,15 +1,19 @@
-import React  from "react";
+import React, {useEffect}  from "react";
 import styles from './CategoryCard.module.css'
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const CategoryCard = ({ shuffledArray }) => {
 
-
+    useEffect(() => {
+        AOS.init();
+        window.addEventListener('load', AOS.refresh);
+      }, []);
 
     return (
         <>
             <div className="row justifiy-content-center align-items-center gy-4 mt-4" >
-                <div className="col-lg-6 col-md-12">
+                <div className="col-lg-6 col-md-12" data-aos="fade-up"  data-aos-duration="800">
 
                     <div className="row gy-4 "  >
                         <div className="col-md-6 col-sm-6" >
@@ -46,9 +50,9 @@ const CategoryCard = ({ shuffledArray }) => {
                         </div>
                     </div>
                 </div>
-                <div className={`col-lg-6 col-md-12  `}>
+                <div className={`col-lg-6 col-md-12`} >
 
-                    <div className={`${styles.imgCat} position-relative`}>
+                    <div className={`${styles.imgCat} position-relative`} data-aos="fade-up" data-aos-delay='400' data-aos-duration="800">
                         <div className={`${styles.overlayCat} `}>
                             <p>{shuffledArray[2].catName}</p>
                         </div>
