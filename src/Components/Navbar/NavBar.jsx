1f008488f8cc291a13446e67d3dd804b4f62ce05
Aya-Adel-Mohamed <aya90318@gmail.com>
2023-09-20 16:11:53 -0700

    Home_Version2

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import styles from "./NavBar.module.css";
 import { useDispatch } from "react-redux";
-import {handleClickOpen } from "../../Redux/Slicies/dialogSlice";
-
+import { handleClickOpen } from "../../Redux/Slicies/dialogSlice";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 export default function NavBar() {
   const [navbar, setNavbar] = useState(false);
   const dispatch = useDispatch()
@@ -22,85 +24,104 @@ export default function NavBar() {
   })
   return (
     <>
-      <nav className={navbar ? `navbar fixed-top navbar-expand-lg py-3 ${styles.colorNav}` : `navbar fixed-top navbar-expand-lg py-3 ${styles.transparent}`}>
-        <div className="container-fluid">
-          <Link to='/' className={styles.logo}>
-            <img src={logo} alt="" className='w-100 ' />
-          </Link>
-          <div className={styles.badgeContainer}>
-            <li className="nav-item me-3 position-relative">
-              <Link className={`nav-link  ${styles.navLink}`} to="favorite">
-                <i className={`fa-sharp fa-solid fa-heart ${styles.size}`}></i>
-                <div className={` ${styles.number}`}><span className={`position-absolute ${styles.num}`}>0</span></div>
-              </Link>
-            </li>
-            <li className="nav-item me-2 position-relative">
-              <Link className={`nav-link ${styles.navLink}`} to="cart">
-                <i className={`fa-solid fa-cart-shopping ${styles.size}`}></i>
-                <div className={` ${styles.number}`}><span className={`position-absolute ${styles.num}`}>0</span></div>
-              </Link>
-            </li>
-          </div>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className={`collapse navbar-collapse ${styles.bgCollapse}`} id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className={`nav-item me-2 ${styles.navItem}`}>
-                <Link className={`nav-link ${styles.navLink} ${styles.navLinkBorder}`} to="home">Home</Link>
-              </li>
-              <li className={`nav-item me-2 ${styles.navItem}`}>
-                <Link className={`nav-link ${styles.navLink} ${styles.navLinkBorder}`} to="shop">Shop</Link>
-              </li>
-
-              <li className={`nav-item dropdown me-2 ${styles.navItem}`}>
-                <Link className={`nav-link dropdown-toggle ${styles.navLink} ${styles.navLinkBorder}`} to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Categories
-                </Link>
-               
-                <ul className={`dropdown-menu ${styles.dropdownMenu}`}>
-                  <li><Link className={`dropdown-item ${styles.item}`} to="#">Science</Link></li>
-                  <li><Link className={`dropdown-item ${styles.item}`} to="#">children</Link></li>
-                  <li><Link className={`dropdown-item ${styles.item}`} to="#">Cooking</Link></li>
-                  <li><Link className={`dropdown-item ${styles.item}`} to="#">Science Fiction</Link></li>
-                  <li><Link className={`dropdown-item ${styles.item}`} to="#">Business</Link></li>
-                  <li><Link className={`dropdown-item ${styles.item}`} to="#">Music</Link></li>
-                  <li><Link className={`dropdown-item ${styles.item}`} to="#">Architecture</Link></li>
-                </ul>
-              </li>
-              <li className={`nav-item me-2 ${styles.navItem}`}>
-                <Link className={`nav-link ${styles.navLink} ${styles.navLinkBorder}`} to="contact">Contact</Link>
-              </li>
-            </ul>
-
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item me-2 px-lg-3">
-                <input type="text" placeholder='search....' className={`form-control rounded-pill ${styles.form}`} />
-              </li>
-
-              <li className="nav-item me-2">
-                <Link className={`nav-link ${styles.navLink}`} to="profile">
-                  <i className={`fa-solid fa-user me-2  ${styles.size}`}></i>
-                  <span className={styles.colorUser}>username</span>
+      <div className="fixed-top">
+            <div className={styles.navTop}>
+              <li className="nav-item me-5 position-relative">
+                <Link className={`nav-link  ${styles.navLinkIcon}`} to="favorite">
+                  <FavoriteBorderOutlinedIcon  sx={{fontSize: {xs:24, sm:24, md:27, lg:24}}}/>
+                  <div className={` ${styles.number}`}>
+                    <span className={`${styles.num}`}>0</span>
+                    </div>
                 </Link>
               </li>
-              
-              <li className="nav-item">
-                <Link
-                  className={`nav-link ${styles.navLink}`} onClick={()=>{dispatch(handleClickOpen({name: "login"}))}}
-                >
-                  Login
+              <li className="nav-item me-5 position-relative">
+                <Link className={`nav-link ${styles.navLinkIcon}`} to="cart">
+      
+                  <ShoppingCartOutlinedIcon sx={{fontSize: {xs:24, sm:24, md:27, lg:24}}} />
+                  <div className={` ${styles.number}`}><span className={` ${styles.num}`}>0</span></div>
                 </Link>
               </li>
-              <li className="nav-item">
-                <button  className={`nav-link ${styles.navLink}`} onClick={()=>{dispatch(handleClickOpen({name: "register"}))}}>
+
+              <li className="nav-item ms-3">
+                  <Link className={`nav-link ${styles.navLinkIcon}`} to="profile">
+                    <PersonOutlineOutlinedIcon sx={{fontSize: {xs:24, sm:24, md:27, lg:26}}} />
+                    <span className={styles.colorUser}>username</span>
+                  </Link>
+                </li>
+            </div>
+        <nav className={navbar ? `navbar navbar-expand-lg ${styles.navBarContainer} ${styles.colorNav}` : `navbar navbar-expand-lg  ${styles.transparent}`}>
+          <div className="container-fluid">
+            <Link to='/' className={styles.logo}>
+              <img src={logo} alt="" className='w-100' />
+            </Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className={`collapse navbar-collapse ${styles.bgCollapse}`} id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className={`nav-item me-2 ${styles.navItem}`}>
+                  <Link className={`nav-link ${styles.navLink} ${styles.navLinkBorder}`} to="home">Home</Link>
+                </li>
+                <li className={`nav-item me-2 ${styles.navItem}`}>
+                  <Link className={`nav-link ${styles.navLink} ${styles.navLinkBorder}`} to="shop">Shop</Link>
+                </li>
+
+                <li className={`nav-item dropdown me-2 ${styles.navItem}`}>
+                  <Link className={`nav-link dropdown-toggle ${styles.navLink} ${styles.navLinkBorder}`} to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Categories
+                  </Link>
+
+                  <ul className={`dropdown-menu ${styles.dropdownMenu}`}>
+                    <li><Link className={`dropdown-item ${styles.item}`} to="#">Science</Link></li>
+                    <li><Link className={`dropdown-item ${styles.item}`} to="#">children</Link></li>
+                    <li><Link className={`dropdown-item ${styles.item}`} to="#">Cooking</Link></li>
+                    <li><Link className={`dropdown-item ${styles.item}`} to="#">Science Fiction</Link></li>
+                    <li><Link className={`dropdown-item ${styles.item}`} to="#">Business</Link></li>
+                    <li><Link className={`dropdown-item ${styles.item}`} to="#">Music</Link></li>
+                    <li><Link className={`dropdown-item ${styles.item}`} to="#">Architecture</Link></li>
+                  </ul>
+                </li>
+                <li className={`nav-item me-2 ${styles.navItem}`}>
+                  <Link className={`nav-link ${styles.navLink} ${styles.navLinkBorder}`} to="contact">Contact</Link>
+                </li>
+              </ul>
+
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+
+
+            
+
+                <li className="nav-item">
+                  <Link
+                    className={`nav-link ${styles.navLink}`} onClick={() => { dispatch(handleClickOpen({ name: "login" })) }}
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <button className={`nav-link ${styles.navLink}`} onClick={() => { dispatch(handleClickOpen({ name: "register" })) }}>
                     Register
-                </button>
+                  </button>
+                </li>
+              </ul>
+            </div>
+            <div className="d-none">
+              <li className="nav-item me-3 position-relative">
+                <Link className={`nav-link  ${styles.navLink}`} to="favorite">
+                  <i className={`fa-sharp fa-solid fa-heart ${styles.size}`}></i>
+                  <div className={` ${styles.number}`}><span className={`position-absolute ${styles.num}`}>0</span></div>
+                </Link>
               </li>
-            </ul>
+              <li className="nav-item me-2 position-relative">
+                <Link className={`nav-link ${styles.navLink}`} to="cart">
+                  <i className={`fa-solid fa-cart-shopping ${styles.size}`}></i>
+                  <div className={` ${styles.number}`}><span className={`position-absolute ${styles.num}`}>0</span></div>
+                </Link>
+              </li>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </>
-)
+  )
 }
--- src/util/util.js
+++ src/util/util.js
@@ -1,5 +1,5 @@
 
 
 // export const baseUrl = `https://book-store-an5l.onrender.com/api/v1/`
-export const baseUrl = `https://book-store-uusp.onrender.com/api/v1/`
+export const baseUrl = `https://codecraftsportfolio.online/api/v1/`
 // export const baseUrl = `http://localhost:8080/api/v1/`   
