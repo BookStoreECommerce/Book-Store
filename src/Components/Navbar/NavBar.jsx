586417a5eb989d8dbdc2bd642e96c36b64bf070c
Aya-Adel-Mohamed <aya90318@gmail.com>
2023-09-25 11:44:14 -0700

    NavBar

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, memo } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import styles from "./NavBar.module.css";
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
-import App from "./App";
 import { RouterProvider, createBrowserRouter, } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./Redux/Store.js";
