00e380541b431be89e5f093a1acc5b642f4387db
WalaaRahman <w.gohary46@gmail.com>
2023-10-09 17:02:03 +0200

    Search + Pagination

--- src/Redux/Slicies/CategoriesAction.js
+++ src/Redux/Slicies/CategoriesAction.js
@@ -1,11 +1,19 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axiosInstance from "../../axios/axios-instance";
 
-export const getCategories = createAsyncThunk("/categories",async (page = 1 , { rejectWithValue })=>{
-
+export const getCategories = createAsyncThunk("/categories",async ({page = 1 , searchTerm  } , { rejectWithValue })=>{
+// let kword= searchTerm || "history";
+console.log(searchTerm);
     try {
-        const {data} = await axiosInstance.get(`category?page=${page}`);
-        return data;
+        if(searchTerm){
+            const {data} = await axiosInstance.get(`category?page=${page}&keyword=${searchTerm}`);
+            return data;
+
+        }
+        else {
+            const {data} = await axiosInstance.get(`category?page=${page}`);
+            return data;
+        }
         
     } catch (error) {
         return rejectWithValue(error.response.data)
--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { getCategories } from "../../Redux/Slicies/CategoriesAction.js";
 import CategoryCard from "../../Components/HomeComponents/CategoryCard/CategoryCard.jsx";
@@ -9,9 +9,11 @@ import Stack from '@mui/material/Stack';
 
 const Categories = () => {
     const { categories } = useSelector((state) => state.cat)
+    const [page, setPageState]=useState(1)
+    const [search, setSearch]=useState("")
 
     let catArray = categories.result;
-    let totalCategoriesCount=categories.totalCount;
+    let totalCategoriesCount = categories.totalCount;
     let totalPages = Math.ceil((totalCategoriesCount / 12));
 
     console.log(totalPages);
@@ -25,34 +27,41 @@ const Categories = () => {
 
 
     useEffect(() => {
-        dispach(getCategories(1));
+        dispach(getCategories({page,searchTerm:search}));
+
     }, [dispach])
-
-    const setPage =(e,p)=>{
-console.log(p);
-dispach(getCategories(p));
+    // Pagination
+    const setPage = (e, p) => {
+        console.log(p);
+        setPageState(p)
+        dispach(getCategories({page:p}));
     }
-
+    // Search
     const searchBooks = (searchKeyword) => {
         console.log(searchKeyword);
+        setSearch(searchKeyword)
+        dispach(getCategories({page,searchTerm:searchKeyword}));
+       
+
     }
+
     const url = `${baseUrl}category?page=1&sort=name&keyword=searchValue&fields=name,image`;
     return (
         <>
             <div className="container mb-3 " style={{ "marginTop": "150px" }}>
-            <div className="py-4">
-            <LiveSearch minCharToSearch="2" label="search categories" url={url} keyword="searchValue" onSubmit={searchBooks} />
-            
-            </div>
-            <div className="row gy-4">
-            {catArray?.map((cat) => (
-                <CategoryCard sectionName="" catName={cat.name} img={cat.image.secure_url} />
-                ))}
+                <div className="py-4">
+                    <LiveSearch minCharToSearch="2" label="search categories" url={url} keyword="searchValue" onSubmit={searchBooks} />
+
+                </div>
+                <div className="row gy-4">
+                    {catArray?.map((cat) => (
+                        <CategoryCard sectionName="" catName={cat.name} img={cat.image.secure_url} />
+                    ))}
                 </div>
 
                 <div className="d-flex flex-column justify-content-center align-items-center mt-3">
                     <Stack spacing={2}>
-                        <Pagination count={totalPages} shape="rounded" onChange={setPage} />
+                        <Pagination count={totalPages} color="primary" shape="rounded" variant="outlined" onChange={setPage} />
                     </Stack>
                 </div>
 
